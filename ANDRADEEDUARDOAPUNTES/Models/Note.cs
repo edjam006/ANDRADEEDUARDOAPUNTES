56d5764ac8e99828e32e8a16a7ab591d6a773498
Leninprog <lenin.arevalo@udla.edu.ec>
2025-06-15 06:36:16 -0500

    Ejecucion correcta en machine

--- ANDRADEEDUARDOAPUNTES/Models/Note.cs
+++ ANDRADEEDUARDOAPUNTES/Models/Note.cs
@@ -11,6 +11,5 @@ namespace ANDRADEEDUARDOAPUNTES.Models
         public string Filename { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
-
     }
 }
