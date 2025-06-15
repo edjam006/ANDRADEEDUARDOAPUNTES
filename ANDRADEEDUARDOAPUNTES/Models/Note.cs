f8b9f62a2d817f74f02f7fb59d4ac1792ff66cb4
Leninprog <lenin.arevalo@udla.edu.ec>
2025-06-15 00:37:32 -0500

    Se muestra correctamente las notas ingresadas.

--- ANDRADEEDUARDOAPUNTES/Models/Note.cs
+++ ANDRADEEDUARDOAPUNTES/Models/Note.cs
@@ -11,5 +11,6 @@ namespace ANDRADEEDUARDOAPUNTES.Models
         public string Filename { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
+
     }
 }
